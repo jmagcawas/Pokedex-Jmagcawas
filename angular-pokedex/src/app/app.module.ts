6ac78a37ff8dde48a898ad19f446cc48149c2e8d
jmagcawas <jorem.magcawas@gmail.com>
2020-04-12 19:19:09 +0800

    ngforerror

---
+++ angular-pokedex/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { MessagesComponent } from './messages/messages.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessagesComponent,
+    PokemonsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-pokedex/src/app/pokemons/pokemons.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.css']
+})
+export class PokemonsComponent implements OnInit {
+
+  
+  pokemons: Pokemon [];
+
+  selectedPokemon: Pokemon;
+
+  constructor(private pokemonService: PokemonService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  getPokemons(): void {
+    this.pokemonService.getPokemon()
+      .subscribe(pokemons => this.pokemons = pokemons);
+  }
+
+  onSelect(pokemon: Pokemon): void {
+    this.selectedPokemon = pokemon;
+    this.messageService.add(`HeroService: Selected hero name=${pokemon.name}`);
+  }
+  
+
+
+}
